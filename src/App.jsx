085b591bcf3b6392e1f705ecb2a9b3307209ff4c
modusami <128193218+modusami@users.noreply.github.com>
2024-04-09 23:27:43 -0400

    Update Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useRoutes } from "react-router-dom";
+import { homePath } from "./data/data";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+
 function App() {
+	let elements = useRoutes([
+		{
+			path: homePath,
+			element: <Home />,
+		},
+		{
+			path: "*",
+			element: <NoPage />,
+		},
+	]);
+
 	return (
-		<div>
-			<h1>hello</h1>
+		<div className="min-h-screen flex justify-center items-center flex-col gap-10">
+			<h1 className="text-[40px]">Alter Ego Creator</h1>
+			<div className="h-[900px] w-[90vw] shadow-2xl">{elements}</div>
 		</div>
 	);
 }
